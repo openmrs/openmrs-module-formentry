f2e0e7f3f7aa23abd50ff80840727c17db9883b5
bwolfe <bwolfe@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2007-11-08 20:20:56 +0000

    Revamping tests in the formentry module to match the new testing system

--- web/src/org/openmrs/module/formentry/web/dynamic/PatientFormEntryController.java
+++ web/src/org/openmrs/module/formentry/web/dynamic/PatientFormEntryController.java
@@ -1,3 +1,16 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.formentry.web.dynamic;
 
 import java.text.DateFormat;
@@ -12,9 +25,9 @@ import org.openmrs.Form;
 import org.openmrs.Location;
 import org.openmrs.User;
 import org.openmrs.api.context.Context;
+import org.openmrs.propertyeditor.LocationEditor;
+import org.openmrs.propertyeditor.UserEditor;
 import org.openmrs.util.OpenmrsConstants;
-import org.openmrs.web.propertyeditor.LocationEditor;
-import org.openmrs.web.propertyeditor.UserEditor;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.beans.propertyeditors.CustomNumberEditor;
 import org.springframework.validation.BindException;
