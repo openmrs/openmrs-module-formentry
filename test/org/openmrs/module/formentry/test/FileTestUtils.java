682b98da97ace1be33c10f56cb1c2c1959ef2431
Ben Wolfe <ben@openmrs.org>
2009-07-28 21:35:20 +0000

    formentry/trunk: Reverting the schema to triplets only - #1404 Author: djazayeri Added fix for utf-8 characters in concept names - #225

--- test/org/openmrs/module/formentry/test/FileTestUtils.java
+++ test/org/openmrs/module/formentry/test/FileTestUtils.java
@@ -1,14 +1,21 @@
 package org.openmrs.module.formentry.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openmrs.module.formentry.PublishInfoPath;
 
 public class FileTestUtils {
 
@@ -55,4 +62,18 @@ public class FileTestUtils {
 			}
 		}
 	}
+	
+	   @Test
+	    public void shouldRemovePreciseNameFromHl7() throws Exception {
+	        String input = "</xsl:attribute>\n" + 
+	            "<xsl:if test=\"obs/history_of_present_illness/patient_already_seen_by_traditional_practitioner/value=&quot;1065^YES^99DCT^1102^YES^99DCT&quot;\">\n" +
+	            "<xsl:attribute name=\"CHECKED\">CHECKED</xsl:attribute>";
+	        String correctOutput = "</xsl:attribute>\n" + 
+	            "<xsl:if test=\"obs/history_of_present_illness/patient_already_seen_by_traditional_practitioner/value=&quot;1065^YES^99DCT&quot;\">\n" +
+	            "<xsl:attribute name=\"CHECKED\">CHECKED</xsl:attribute>";
+	        StringWriter sw = new StringWriter();
+	        PublishInfoPath.removeConceptNamesInXslHelper(new BufferedReader(new StringReader(input)), new PrintWriter(sw));
+	        System.out.println("We got: \n" + sw.toString());
+	        assertEquals(correctOutput, sw.toString().trim());
+	    }
 }
