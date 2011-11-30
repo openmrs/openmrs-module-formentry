f294b9f7fa39b6361d371804b9d8bed274fe6c88
Kishore Kumar Yekkanti <kishoreyekkanti@gmail.com>
2011-11-30 18:39:45 +0000

    Introduced the ability to run release tests on  bundled modules with any specified version of OpenMRS

---
+++ release-tests/src/main/java/org/openmrs/stories/LookupFormentryInformation.java
@@ -0,0 +1,32 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ * <p/>
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * <p/>
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.stories;
+
+import org.openmrs.Steps;
+import org.openmrs.Story;
+import org.openmrs.steps.AdminSteps;
+import org.openmrs.steps.LoginSteps;
+import org.openmrs.steps.LookupFormentryInformationSteps;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class LookupFormentryInformation extends Story {
+
+    @Override
+    public List<Steps> includeSteps() {
+        return asList(new LoginSteps(driver), new AdminSteps(driver), new LookupFormentryInformationSteps(driver));
+    }
+}
